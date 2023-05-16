a1ef3d2b9c89f640f2b1a47c1233507102800fed
cheehengk <cheehengk@yahoo.com.sg>
2023-05-16 16:36:59 +0700

    Update scripting stuff

--- scripter.py
+++ scripter.py
@@ -7,24 +7,19 @@ openai.api_key = "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"
 
 def extract_pdf(path):
     pdfReader = PdfReader(path)
-    num_pages = len(pdfReader.pages)
-    print("Number of Pages: ", num_pages)
+    pages = len(pdfReader.pages)
+    print("Number of Pages: ", pages)
 
     text_data = []
 
-    for i in range(num_pages):
+    for i in range(pages):
         text_data.append(pdfReader.pages[i].extract_text())
 
-    return text_data, num_pages
+    return text_data, pages
 
 
-def generate_video_script(text_data, page, pages):
-    if page == 0:
-        prompt = "Give me a short video transcript: " + text_data
-    elif page == pages:
-        prompt = "Give me a short video transcript: " + text_data
-    else:
-        prompt = "Give me a short video transcript: " + text_data
+def generate_video_script(text_data):
+    prompt = "Give me a short video transcript: " + text_data
 
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -45,6 +40,6 @@ script = []
 
 for i in range(num_pages):
     print("Page ", i + 1)
-    script.append(generate_video_script(text[i], i, num_pages))
+    script.append(generate_video_script(text[i]))
     print(script[i])
     # time.sleep(3)  # 3 seconds delay to cool down API calls
