261228cd1f821406f918210cce9751d8823b8934
cheehengk <cheehengk@yahoo.com.sg>
2023-05-16 14:31:05 +0700

    Add summary chain to llmchain_test

--- llmchain_test.py
+++ llmchain_test.py
@@ -24,3 +24,17 @@ while question != "":  # enter blank line to exit
     result = llm_chain.predict(question=question)
     print(result, "\n")
     question = input("Enter next question: ")
+
+# Generating summary with new chain using the old memory
+summary_template = "{chat_history} Summarise our conversation in {words} words or less"
+summary_prompt = PromptTemplate(
+    input_variables=["chat_history", "words"],
+    template=summary_template)
+summary_chain = LLMChain(
+    llm=llm,
+    prompt=summary_prompt,
+    memory=memory,
+)
+print("Here is a summary of our conversation:")
+summary_result = summary_chain.predict(words="10")
+print(summary_result)
