a3c3d5bda8e942547ccde35aa3173e9e4ef72ec3
cheehengk <cheehengk@yahoo.com.sg>
2023-05-19 16:38:43 +0700

    Add flask

---
+++ flask_app/app.py
@@ -0,0 +1,83 @@
+import os
+from flask import Flask, render_template, make_response, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from src.scripter import main
+from threading import Thread
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'pdf'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_PATH'] = 1000000  # in bytes
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/')
+def index():
+    return make_response(render_template('index.html', response=''))
+
+
+@app.route('/error/<err>')
+def error(err):
+    if err == 'invalid-file-type':
+        err = 'Invalid File Type!'
+    elif err == 'file-count-error':
+        err = 'Wrong number of files in upload directory!'
+    elif err == 'conversion-failure':
+        err = 'Conversion failure!'
+    else:
+        err = 'Error!'
+
+    return make_response(render_template('index.html', message=err))
+
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        if f and allowed_file(f.filename):
+            f.filename = 'user_upload.pdf'
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+            return 'file uploaded successfully'
+        else:
+            return redirect('/error/invalid-file-type')
+
+
+@app.route('/loading')
+def loading():
+    thread = Thread(target=ai_conversion())
+    thread.start()
+
+    return make_response(render_template('video.html'))
+
+
+def ai_conversion():
+    dir_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'uploads')
+    file_count = 0
+    # Iterate directory
+    for path in os.listdir(dir_path):
+        if os.path.isfile(os.path.join(dir_path, path)):
+            file_count += 1
+
+    if not file_count == 1:
+        return redirect('/error/file-count-error')
+
+    # AI Conversion here
+    response = main()
+
+    if not response == 0:
+        return redirect('/error/conversion-failure')
+
+    assert response == 0
+
+    return redirect('/complete')
+
+
+@app.route('/complete')
+def complete():
+    return 'complete!'
---
+++ flask_app/src/file_cleaner.py
@@ -0,0 +1,66 @@
+import os
+import shutil
+
+PARENT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+IMAGE_PATH = os.path.join(PARENT_PATH, 'src/temp_assets/Images')
+AUDIO_PATH = os.path.join(PARENT_PATH, 'src/temp_assets/Audios')
+VIDEO_PATH = os.path.join(PARENT_PATH, 'src/temp_assets/Videos')
+TEXT_PATH = os.path.join(PARENT_PATH, 'src/temp_assets/Texts')
+
+
+def clean_images():
+    for filename in os.listdir(IMAGE_PATH):
+        file_path = os.path.join(IMAGE_PATH, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+
+def clean_audios():
+    for filename in os.listdir(AUDIO_PATH):
+        file_path = os.path.join(AUDIO_PATH, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+
+def clean_videos():
+    for filename in os.listdir(VIDEO_PATH):
+        file_path = os.path.join(VIDEO_PATH, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+
+def clean_texts():
+    for filename in os.listdir(TEXT_PATH):
+        file_path = os.path.join(TEXT_PATH, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+
+def cleanup():
+    clean_images()
+    clean_audios()
+    clean_videos()
+    clean_texts()
+    print("Intermediary assets cleaned up.")
+
+# cleanup()
