e6f691bf84f93ad665e35d8728299bed6e179b2d
cheehengk <cheehengk@yahoo.com.sg>
2023-05-30 17:12:44 +0700

    Revert to flask and rq compose

--- Dockerfile
+++ Dockerfile
@@ -27,4 +27,4 @@ COPY . /app
 # Copy restriction policy to ImageMagick
 COPY policy.xml /etc/ImageMagick-6/
 
-CMD ["python", "worker.py"]
+CMD ["flask", "run"]
--- worker.py
+++ worker.py
@@ -1,34 +1,31 @@
 import os
-from flask import Flask
+
 from redis import Redis
 from rq import Worker, Queue, Connection
 from flask_app.src.scripter import main
 
-
-app = Flask(__name__)
 # load_dotenv()
 redis_host = os.environ.get("REDIS_HOST")
 redis_port = os.environ.get("REDIS_PORT")
 redis_pw = os.environ.get("REDIS_PW")
 
-redis_conn = Redis(
-    host=redis_host,
-    port=int(redis_port),
-    password=redis_pw
-)
-
-
-@app.route('/')
-def process_tasks():
-    listen = ['default']
-
-    with Connection(connection=redis_conn):
-        queues = [Queue(queue_name, connection=redis_conn) for queue_name in listen]
-        worker = Worker(queues)
-        worker.work()
-
-    return 'Worker started successfully.'
-
+listen = ['default']
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    redis_conn = Redis(
+        host=redis_host,
+        port=int(redis_port),
+        password=redis_pw
+    )
+    # redis_conn = Redis(host='redis', port=6379, db=0)
+    with Connection(connection=redis_conn):
+        worker = Worker(map(Queue, listen))
+
+        # @worker.perform_job
+        # def perform_job(job):
+        #     try:
+        #         return job.perform()
+        #     except Exception as e:
+        #         return f'Error occurred: {str(e)}'
+
+        worker.work()
